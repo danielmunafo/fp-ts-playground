04f4608f73f22e63560e6a4ebe975f4699624982
Daniel Tostes <dann.tostes@gmail.com>
2024-05-21 15:22:02 +0200

    Refactor InMemoryUserRepository
    - Improve cohesion of save method by allowing updates
    
    - Use Map instead of Array as storage

--- src/core/port/UserRepositoryPort.ts
+++ src/core/port/UserRepositoryPort.ts
@@ -3,6 +3,6 @@ import { TaskEither } from "fp-ts/TaskEither";
 import { User } from "../domain";
 
 export type UserRepositoryPort = {
-  save: (user: User) => TaskEither<Error, void>;
+  save: (user: User) => TaskEither<Error, string>;
   findByEmail: (email: string) => TaskEither<Error, Option<User>>;
 };
