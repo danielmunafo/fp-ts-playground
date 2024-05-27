b939788609d17a095e1ac291677fe3aed9f6ede8
Daniel Tostes <dann.tostes@gmail.com>
2024-05-27 14:44:35 +0200

    Refactor UserEntity
    - Use common types directory

--- packages/core/src/domain/entities/UserEntity.ts
+++ packages/core/src/domain/entities/UserEntity.ts
@@ -1,40 +1,6 @@
 import * as E from "fp-ts/Either";
-import { pipe } from "fp-ts/function";
 import * as t from "io-ts";
-import { withMessage } from "io-ts-types";
-
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-const Email = withMessage(
-  new t.Type<string, string, unknown>(
-    "Email",
-    (u): u is string => typeof u === "string",
-    (u, c) =>
-      pipe(
-        t.string.validate(u, c),
-        E.chain((s) => (emailRegex.test(s) ? t.success(s) : t.failure(u, c))),
-      ),
-    t.identity,
-  ),
-  () => "Invalid email",
-);
-
-const uuidV4Regex =
-  /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-
-const UUIDv4 = withMessage(
-  new t.Type<string, string, unknown>(
-    "UUIDv4",
-    (u): u is string => typeof u === "string",
-    (u, c) =>
-      pipe(
-        t.string.validate(u, c),
-        E.chain((s) => (uuidV4Regex.test(s) ? t.success(s) : t.failure(u, c))),
-      ),
-    t.identity,
-  ),
-  () => "Invalid UUID v4",
-);
+import { Email, UUIDv4 } from "../../common";
 
 const UserCodec = t.type({
   id: UUIDv4,
--- packages/core/src/domain/entities/index.ts
+++ packages/core/src/domain/entities/index.ts
@@ -1,2 +1,4 @@
+export * from "./HeartRateEntity";
+export * from "./HeartRateEntityArb";
 export * from "./UserEntity";
 export * from "./UserEntityArb";
